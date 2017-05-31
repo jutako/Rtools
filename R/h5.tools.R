232724802cdfe2b7faa0cde09a4fa4adad24b366
Jussi Korpela <jussitapiokorpela@gmail.com>
2017-05-31 10:18:24 +0300

    Added roxygen2 documentation.

--- R/h5.tools.R
+++ R/h5.tools.R
@@ -1,11 +1,22 @@
 ## Tools to read FIOH BWRC HDF5 data files
 
-require('h5')
-
-# Load data from HDF5 file
+#' Load data from HDF5 file
+#'
+#' @description
+#' Load data from HDF5 file
+#'
+#' @param infile character, Full path to the h5 file
+#' @param element character, h5 data element to load
+#' 
+#' @return Element 'element' from file as a numeric array
+#'
+#' @importFrom h5 h5file
+#' @importFrom h5 h5close
+#'
+#' @export
 load.h5.array <- function(infile, element){
   
-  f <- h5file(infile, 'r')
+  f <- h5::h5file(infile, 'r')
   
   ndim <- length(attr(f[element],'maxdim'))
   if (ndim==1){
@@ -16,20 +27,35 @@ load.h5.array <- function(infile, element){
     data = d3h5_to_array(f, element)
   } else (stop())
   
-  h5close(f)
+  h5::h5close(f)
   
   data
 }
 
 
-# Load all average ERPs from HDF5 file
-# Note: assumes structure /erpavg/<dataset id> use loaddir.h5*() to load from single files.
+#' Load all average ERPs from HDF5 file
+#'
+#' @description
+#' Load all average ERPs from HDF5 file
+#' Note: assumes structure /erpavg/<dataset id> use loaddir.h5*() to 
+#' load from single files.
+#'
+#' @param infile character, Full file name of the ERP HDF5 file
+#' 
+#' @return A list of erpavg data matrices
+#'
+#' @importFrom h5 h5file
+#' @importFrom h5 h5close
+#' @importFrom h5 list.groups
+#' @importFrom h5 list.datasets
+#'
+#' @export
 load.h5.erpavg <- function(infile){
   
-  f <- h5file(infile, 'r')
+  f <- h5::h5file(infile, 'r')
   
-  if ('/erpavg' %in% list.groups(f)){
-    ds.arr <- list.datasets(f['/erpavg'])  
+  if ('/erpavg' %in% h5::list.groups(f)){
+    ds.arr <- h5::list.datasets(f['/erpavg'])  
   } else {
     ds.arr <- '/erpavg'
   }
@@ -41,21 +67,36 @@ load.h5.erpavg <- function(infile){
     rm(data)
   }
   
-  h5close(f)
+  h5::h5close(f)
   
   names(dlst) <- basename(ds.arr)
   dlst
 } 
 
 
-# Load all single-trial ERPs from HDF5 file
-# Note: assumes structure /erp/<dataset id> use loaddir.h5*() to load from single files.
+#' Load all single-trial ERPs from HDF5 file
+#'
+#' @description
+#' Load all single-trial ERPs from HDF5 file
+#' Note: assumes structure /erp/<dataset id> use loaddir.h5*() to 
+#' load from single files.
+#'
+#' @param infile character, Full file name of the ERP HDF5 file
+#' 
+#' @return A list of single-trial ERP data matrices
+#'
+#' @importFrom h5 h5file
+#' @importFrom h5 h5close
+#' @importFrom h5 list.groups
+#' @importFrom h5 list.datasets
+#' 
+#' @export
 load.h5.erp <- function(infile){
   
-  f <- h5file(infile, 'r')
+  f <- h5::h5file(infile, 'r')
   
-  if ('/erp' %in% list.groups(f)){
-    ds.arr <- list.datasets(f['/erp'])  
+  if ('/erp' %in% h5::list.groups(f)){
+    ds.arr <- h5::list.datasets(f['/erp'])  
   } else {
     ds.arr <- '/erp'
   }
@@ -67,19 +108,31 @@ load.h5.erp <- function(infile){
     rm(data)
   }
   
-  h5close(f)
+  h5::h5close(f)
   
   names(dlst) <- basename(ds.arr)
   dlst
 } 
 
 
-# Load data from all hdf5 files in a directory, output a list
+#' Load data from all hdf5 files in a directory, output a list
+#'
+#' @description
+#' Load data from all hdf5 files in a directory, output a list
+#'
+#' @param indir character, Directory name
+#' @param element character, Data element to load
+#' @param pattern character, File name pattern to use
+#' 
+#' @return A list of data matrices
+#' 
+#' @examples 
+#' indir <- '~/ukonkuva/projects/SeamlessCare_2015-16/analysis/ctap_branch/erp/prepro/ica/ERP_nback_allNTrg/this/figures/CTAP_plot_ERP/set6_fun3/'
+#' pattern <- '.*NB01.*_ERPdata.h5'
+#' element <- '/erp'
+#' 
+#' @export
 loaddir.h5 <- function(indir, element, pattern = '*.h5'){
-  
-  #indir <- '~/ukonkuva/projects/SeamlessCare_2015-16/analysis/ctap_branch/erp/prepro/ica/ERP_nback_allNTrg/this/figures/CTAP_plot_ERP/set6_fun3/'
-  #pattern <- '.*NB01.*_ERPdata.h5'
-  #element <- '/erp'
   
   fname.arr <- list.files(indir, pattern = pattern, full.names = F) #list hdf5 files
   if (length(fname.arr)==0){
@@ -93,12 +146,25 @@ loaddir.h5 <- function(indir, element, pattern = '*.h5'){
 }
 
 
-# Load ERP data from all hdf5 files in a directory, output a list
+#' Load ERP data from all hdf5 files in a directory, output a list
+#'
+#' @description
+#' Load ERP data from all hdf5 files in a directory, output a list.
+#' Uses loaddir.h5() and adds some metadata on top.
+#'
+#' @param indir character, Directory name
+#' @param element character, Data element to load
+#' @param pattern character, File name pattern to use
+#' 
+#' @return A list of data matrices
+#' 
+#' @examples 
+#' indir <- '~/ukonkuva/projects/SeamlessCare_2015-16/analysis/ctap_branch/erp/prepro/ica/ERP_nback_allNTrg/this/figures/CTAP_plot_ERP/set6_fun3/'
+#' pattern <- '.*NB01.*_ERPdata.h5'
+#' element <- '/erp'
+#' 
+#' @export
 loaddir.h5.erp <- function(indir, element, pattern = '*_ERPdata.h5'){
-  
-  #indir <- '~/ukonkuva/projects/SeamlessCare_2015-16/analysis/ctap_branch/erp/prepro/ica/ERP_nback_allNTrg/this/figures/CTAP_plot_ERP/set6_fun3/'
-  #pattern <- '.*NB01.*_ERPdata.h5'
-  #element <- '/erp'
   
   # Load data
   dl <- loaddir.h5(indir, element, pattern)
@@ -118,9 +184,17 @@ loaddir.h5.erp <- function(indir, element, pattern = '*_ERPdata.h5'){
 }
 
 
-# Transform HDF5 numericdata to array
-
-# vector
+#' Transform HDF5 numeric vector to array
+#'
+#' @description
+#' Transform HDF5 numeric vector to array
+#'
+#' @param h5handle HDF5 file handle, Get handle using h5::h5file(infile, 'r')
+#' @param dpath character, Path to the HDF5 vector like data element
+#' 
+#' @return Data in 'dpath' as a numeric array
+#'
+#' @export
 d1h5_to_array <- function(h5handle, dpath){
   
   d1ID <-  h5attr2dimname( h5attr(h5handle[dpath], 'd1ID') )
@@ -130,7 +204,20 @@ d1h5_to_array <- function(h5handle, dpath){
   data
 }
 
-# matrix
+
+#' Transform HDF5 numeric matrix to array
+#'
+#' @description
+#' Transform HDF5 numeric matrix to array
+#'
+#' @param h5handle HDF5 file handle, Get handle using h5::h5file(infile, 'r')
+#' @param dpath character, Path to the HDF5 matrix like data element
+#' 
+#' @return Data in 'dpath' as a numeric array
+#'
+#' @importFrom h5 h5attr
+#'
+#' @export
 d2h5_to_array <- function(h5handle, dpath){
   
   attr.arr <- list.attributes(h5handle[dpath])
@@ -150,7 +237,20 @@ d2h5_to_array <- function(h5handle, dpath){
   data
 }
 
-# 3 dimensional array
+
+#' Transform HDF5 3D numeric matrix to array
+#'
+#' @description
+#' Transform HDF5 3D numeric matrix to array
+#'
+#' @param h5handle HDF5 file handle, Get handle using h5::h5file(infile, 'r')
+#' @param dpath character, Path to the HDF5 3D matrix like data element
+#' 
+#' @return Data in 'dpath' as a numeric array
+#'
+#' @importFrom h5 h5attr
+#'
+#' @export
 d3h5_to_array <- function(h5handle, dpath){
   
   d1ID <-  h5attr2dimname( h5attr(h5handle[dpath], 'd1ID') )
@@ -162,9 +262,20 @@ d3h5_to_array <- function(h5handle, dpath){
   data
 }
 
-# Convert HDF5 attribute to dimnames() combatible format
-# Note: HDF5 representation of char arrays / cellstr is user defined.
-# Using the convention of collapsing into single string with ";" separators.
+
+#' Convert HDF5 attribute to dimnames() compatible format
+#'
+#' @description
+#' Convert HDF5 attribute to dimnames() combatible format
+#' Note: HDF5 representation of char arrays / cellstr is user defined.
+#' Using the convention of collapsing into single string with ";" 
+#' separators.
+#'
+#' @param attrdata HDF5 attribute data, Output of h5::h5attr(object, attributename)
+#'
+#' @return Attribute data in dimnames() compatible format
+#'
+#' @export
 h5attr2dimname <- function(attrdata){
   if (is.character(attrdata)){
     attrdata <- strsplit(attrdata, ';')[[1]]
@@ -172,23 +283,43 @@ h5attr2dimname <- function(attrdata){
   attrdata
 }
 
-# Fetch some attribut from a HDF5 file
+
+#' Fetch some attribute from a HDF5 file
+#'
+#' @description
+#' Fetch some attribute from a HDF5 file
+#'
+#' @param hdf5file string, Full filename of the HDF5 file, e.g. 
+#' @param dgroup string, Data set group name, e.g. 
+#' @param dataset string, Data set name, e.g. 
+#' @param attrib string, Data attribute name
+#'
+#' @return Attribute data
+#'
+#' @importFrom h5 h5file
+#' @importFrom h5 h5close
+#' @importFrom h5 list.groups
+#' @importFrom h5 list.datasets
+#'
+#' @examples
+#' hdf5file = '/home/jkor/tmp/test.h5'
+#' dgroup <- '/erp'
+#' dataset <- 'SEAMPILOT301'
+#' attrib <- 'maxdim'
+#'
+#' @export
 h5.get.dataset.attr <- function(hdf5file, dgroup, dataset, attrib){
-  # hdf5file = '/home/jkor/tmp/test.h5'
-  # dgroup <- '/erp'
-  # dataset <- 'SEAMPILOT301'
-  # attrib <- 'maxdim'
-  
-  f <- h5file(hdf5file, 'r')
+ 
+  f <- h5::h5file(hdf5file, 'r')
   
   # List available datasets in the desired group
-  grps <- list.groups(f)
+  grps <- h5::list.groups(f)
   
   if (is.null(dgroup)){
     ds <- dataset
   } else {
     match <- grps %in% dgroup
-    ds <- list.datasets(f[grps[match]])
+    ds <- h5::list.datasets(f[grps[match]])
   }
   
   # Get attribute
@@ -206,7 +337,7 @@ h5.get.dataset.attr <- function(hdf5file, dgroup, dataset, attrib){
     } 
   }
 
+  h5::h5close(f)
   
-  h5close(f)
   out
 }
--- R/rtools.R
+++ R/rtools.R
@@ -1,18 +1,96 @@
+# Miscellaneous Rtools that I find useful
+
+#' Get file extension from filename
+#'
+#' @description
+#' Get file extension from filename
+#'
+#' @param filename character, Full file name 
+#'
+#' @return File extension from filename
+#' 
+#' @examples
+#' file.ext('a.b')
+#'
+#' @export
+file.ext <- function(filename){
+  splits <- strsplit(basename(filename), '\\.')[[1]]
+  splits[length(splits)]
+}
 
 
-#' Circularly shift vector elemens
+#' CTAP/BWRC savename parsing
+#'
+#' @description
+#' Parse a CTAP/BWRC casename, often used in filenames when saving data.
+#'
+#' @param fname character, Full file name
+#'
+#' @return A list of elements parsed from file name
+#'
+#' @export
+parse.savename <- function(fname){
+
+  fpath <- dirname(fname) #just path
+  fname <- basename(fname) #filename only
+  
+  splits <- strsplit(fname,'_')[[1]]
+  casename <- paste(splits[1:4], collapse = '_')
+  notCasename <- paste(splits[5:length(splits)], collapse = '_')
+  subject <- splits[1]
+  part <- splits[2]
+  session <- splits[3]
+  measurement <- splits[4]
+  task <- splits[5]
+  erpid <- splits[6]
+  
+  list(path = fpath, filename = fname,
+       casename = casename, notcasename = notCasename,
+       subject = subject, part = part, session = session, measurement = measurement,
+       task = task, erpid = erpid)
+}
+
+
+#' Circularly shift vector elements
+#'
+#' @description
 #' To e.g. change the position of a peak within a series
 #' positive n shifts things to the left, negative to the right
 #' 
 #' From: http://stackoverflow.com/questions/30542128/circular-shifting-arrays-in-r-by-distance-n
+#'
+#' @param x [1,m] numeric, input data
+#' @param n [1,1] ind, number of positions to shift,
+#'          positive n shifts things to the left, negative to the right 
+#'
+#' @return A shifted version of x
+#'
+#' @export
 shifter <- function(x, n) {
   if (n == 0) x else c(tail(x, -n), head(x, n))
 }
 
 
 #' Transform a matrix into a ggplottable data.frame
-make.ggplot.df <- function(dmat, dataset = NULL, variable2numeric = T){
+#' 
+#' @description
+#' Converts a numeric matrix into a ggplot compatible data.frame.
+#'
+#' @param dmat [n,m] numeric, input data matrix
+#' @param dataset character, data set id string
+#' @param variable2numeric logical, Should $variable be converted to numeric?
+#'
+#' @return A data.frame representation of dmat
+#'
+#' @importFrom reshape2 melt
+#'
+#' @export
+make.ggplot.df <- function(dmat,
+                           dataset = NULL,
+                           variable2numeric = T){
+  
   pd <- as.data.frame(dmat)
+  
   if ( !is.null(dimnames(dmat)) ){
     pd$row <- dimnames(dmat)[[1]]
   } else {
